47c38bc153c6002191ba2e6ce4e5d5e0d1814c0e
korewadavid <korewadavid@gmail.com>
2024-05-19 23:11:24 +0200

    started ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damendez <damendez@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 21:27:00 by damendez          #+#    #+#             */
+/*   Updated: 2024/05/19 21:27:00 by damendez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+#include "contact.hpp"
+
+/*
+ * 1. Initialize input string and needed classes
+ * 1. Get user input (start-up output and input error output)
+ * 2. Call phonebook method based on input (ADD, SEARCH, EXIT)
+*/
+int main(void)
+{
+
+}
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damendez <damendez@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 22:48:33 by damendez          #+#    #+#             */
+/*   Updated: 2024/05/19 22:48:33 by damendez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+#include <string>
+
+#include "contact.hpp"
